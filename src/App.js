d1c8a0fddf0fbd454b1c93157c1ec5496f73bb98
Holden Beale <itdbealh@aikencounty.net>
2019-06-27 14:04:19 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FullWidthGrid from './components/fullWidthGrid.js'
+
+function App() {
+  return (
+    <div className="App">
+      <FullWidthGrid />
+    </div>
+  );
+}
+
+export default App;
