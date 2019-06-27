6d8a38a46b53c0825d3d50b0984d850f02511086
Holden Beale <itdbealh@aikencounty.net>
2019-06-27 16:27:46 -0400

    containers

--- src/components/fullWidthGrid.js
+++ src/components/fullWidthGrid.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-import ButtonAppBar from './buttonAppBar.js'
-import ImgCard from './imgCard.js'
-import MediaCard from './mediaCard.js'
+import ButtonAppBar from "./buttonAppBar.js";
+import ImgCard from "./imgCard.js";
+import MediaCard from "./mediaCard.js";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   },
   radioGrid: {
-    alignItems: 'right',
-    alignContent: 'right'
+    alignItems: "right",
+    alignContent: "right"
   }
 }));
 
@@ -27,17 +27,19 @@ export default function FullWidthGrid() {
 
   return (
     <div className={classes.root}>
-        <Grid container spacing={3}>
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item xs={8} direction = "row">
+        <Grid item xs={8} direction="row">
           <ButtonAppBar />
         </Grid>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
@@ -50,7 +52,9 @@ export default function FullWidthGrid() {
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
@@ -60,20 +64,24 @@ export default function FullWidthGrid() {
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item xs={6} > 
+        <Grid item xs={6}>
           <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item xs={2} > 
+        <Grid item xs={2}>
           <Paper className={classes.paper}>radio</Paper>
         </Grid>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
@@ -81,19 +89,17 @@ export default function FullWidthGrid() {
           <MediaCard />
         </Grid>
         <Grid item xs={2}>
-        <MediaCard />
+          <MediaCard />
         </Grid>
         <Grid item xs={2}>
-        <MediaCard />
+          <MediaCard />
         </Grid>
         <Grid item xs={2}>
-        <MediaCard />
+          <MediaCard />
         </Grid>
-        
-        <Grid item xs={2}>
-          <Paper className={classes.paper}></Paper>
-        </Grid>
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
@@ -106,10 +112,14 @@ export default function FullWidthGrid() {
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+        
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+        
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
@@ -131,11 +141,12 @@ export default function FullWidthGrid() {
         <Grid item xs={2}>
           <Paper className={classes.paper}></Paper>
         </Grid>
+      </Grid>
 
+      <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.paper}> text </Paper>
         </Grid>
-
       </Grid>
     </div>
   );
