86d8a78415ddc46279f591e05e7bd1053b807ac9
Holden Beale <itdbealh@aikencounty.net>
2019-06-27 15:10:05 -0400

    some wrangling

---
+++ src/components/buttonAppBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import SimpleMenu1 from './menu1.js'
+import SimpleMenu2 from './menu2.js'
+import SimpleMenu3 from './menu3.js'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left'
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            text
+          </Typography>
+          <SimpleMenu1 />
+          <SimpleMenu2 />
+          <SimpleMenu3 />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- src/components/fullWidthGrid.js
+++ src/components/fullWidthGrid.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
+import ButtonAppBar from './buttonAppBar.js'
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
@@ -19,27 +21,58 @@ export default function FullWidthGrid() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+        <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        <Grid item xs={8} direction = "row">
+          <ButtonAppBar />
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+
+        <Grid item xs={2}>
+          <Paper className={classes.paper}></Paper>
         </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        <Grid item xs={5}>
+          <Paper className={classes.paper}>text</Paper>
         </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>img</Paper>
         </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
+        </Grid>
+
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>xs=2 col</Paper>
         </Grid>
       </Grid>
     </div>
